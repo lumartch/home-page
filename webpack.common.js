c63d13728b47943c577bc51ac1e8041c8e3dd049
Lumart Chryssomallis <lumartch@gmail.com>
2023-03-06 16:02:02 -0600

    Changed build command for webpack

--- webpack.common.js
+++ webpack.common.js
@@ -32,7 +32,7 @@ const common = {
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
   },
   plugins: [
     new HtmlWebpackPlugin({
