7a94a7bd125af4d363ba55051c89c980bfdbcc47
Lumart Chryssomallis <lumartch@gmail.com>
2023-03-17 16:37:12 -0600

    Added favico to the project.

--- src/index.css.ts
+++ src/index.css.ts
@@ -2,6 +2,7 @@ import { style } from '@vanilla-extract/css';
 
 export const IndexStyle = style({
 	backgroundColor: '#1A2027',
-    height: '100%',
+    height: '100vh',
     width: '100%',
+    overflowY: 'scroll',
 });
