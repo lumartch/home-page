f2272f705bb7783b30099df8ba30f1eefe589426
Lumart Chryssomallis <lumartch@gmail.com>
2023-05-03 11:21:01 -0600

    Removed About

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Grid, Typography, Link } from '@mui/material';
 
-import { GridStyle } from './About.css';
+import { GridStyle, IFrameStyle, aLink } from './About.css';
 import { DevInfo } from '../../util';
-import { IFrame } from '../iframe/IFrame';
 
 export const About = () => {
     return (
-        <Grid className={GridStyle} container spacing={2}>
+        <Grid className={GridStyle} container spacing={2} item xs={12} md={2}>
             <Grid item xs={12}>
                 <Typography variant='h5'>ABOUT</Typography>
             </Grid>
@@ -68,7 +67,12 @@ export const About = () => {
                     </Typography>
                 </Grid>
                 <Grid item xs={12}>
-                    <IFrame />
+                    <>
+                        <iframe width="100%" height="300" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1438723912&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+                        <div className={IFrameStyle}>
+                            <a className={aLink} href="https://soundcloud.com/lumartch" title="Lumart Chryssomallis" rel="noreferrer" target="_blank" >Lumart Chryssomallis</a> · <a className={aLink} href="https://soundcloud.com/lumartch/best-of-you-foo-fighters-realistic-string-quartet" title="Best of you - Foo Fighters (Realistic String Quartet)" rel="noreferrer" target="_blank">Best of you - Foo Fighters (Realistic String Quartet)</a>
+                        </div>
+                    </>
                 </Grid>
             </Grid>
         </Grid>
--- src/pages/main/Main.css.ts
+++ src/pages/main/Main.css.ts
@@ -3,3 +3,8 @@ import { style } from '@vanilla-extract/css';
 export const MainStyle = style({
 	backgroundColor: 'transparent',
 });
+
+export const RightBanner = style({
+	position: 'fixed',
+	right: '120px'
+});
